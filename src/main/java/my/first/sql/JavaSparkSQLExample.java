778a6a0974bc1a85c5a029b59d2ee9b531220a76
lionhuo <huopan820@gmail.com>
2016-11-21 18:00:13 +0800

    add

--- src/main/java/my/first/sql/JavaSparkSQLExample.java
+++ src/main/java/my/first/sql/JavaSparkSQLExample.java
@@ -86,7 +86,7 @@ public class JavaSparkSQLExample {
   public static void main(String[] args) {
     // $example on:init_session$
     SparkSession spark = SparkSession
-      .builder().master("local")//                  .master("spark://192.168.1.26:7077")
+      .builder().master("local")//.master("spark://192.168.1.26:7077")
       .appName("Java Spark SQL basic example")
       .config("spark.some.config.option", "some-value")
       .getOrCreate();
